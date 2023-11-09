ca641504ee89ce9ba61f6d9639eb8a27ec7ef8ba
HossamAhmedHassan <h1o2s3s4a5m6.h@gmail.com>
2023-11-09 13:57:28 +0200

    I made model connected it with admin show it in templates

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    res_choices = [
+        ('restaurant1','restaurant1'),
+        ('restaurant2','restaurant2'),
+        ('restaurant3','restaurant3'),
+        ('restaurant4','restaurant4'),
+        ('restaurant5','restaurant5'),
+        ('restaurant6','restaurant6'),
+    ]
+    title       = models.CharField(max_length=50)
+    description = models.TextField(blank=True,null=True)
+    price       = models.DecimalField(max_digits=6,decimal_places=2)
+    image       = models.ImageField(upload_to='photos/%y/%m/%d')
+    restaurant  = models.CharField(max_length=50,choices=res_choices)
+    active      = models.BooleanField()
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def respage(request):
@@ -13,26 +14,28 @@ def cart(request):
     
     return render(request,'cart/cart.html')
 
+context = {'pro':Product.objects.all()}
+
 def restaurant1(request):
     
-    return render(request,'restaurants/restaurant1.html')
+    return render(request,'restaurants/restaurant1.html',context)
 
 def restaurant2(request):
     
-    return render(request,'restaurants/restaurant2.html')
+    return render(request,'restaurants/restaurant2.html',context)
 
 def restaurant3(request):
     
-    return render(request,'restaurants/restaurant3.html')
+    return render(request,'restaurants/restaurant3.html',context)
 
 def restaurant4(request):
     
-    return render(request,'restaurants/restaurant4.html')
+    return render(request,'restaurants/restaurant4.html',context)
 
 def restaurant5(request):
     
-    return render(request,'restaurants/restaurant5.html')
+    return render(request,'restaurants/restaurant5.html',context)
 
 def restaurant6(request):
     
-    return render(request,'restaurants/restaurant6.html')
+    return render(request,'restaurants/restaurant6.html',context)
