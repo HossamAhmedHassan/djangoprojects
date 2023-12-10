708e0954591031d86bea2fc7f6adca6b023da167
HossamAhmedHassan <h1o2s3s4a5m6.h@gmail.com>
2023-12-10 21:08:12 +0200

    the end of the end

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product,Movingdata,Movingtoorders,MovedData
-from django.http import HttpResponseNotFound,JsonResponse
+from django.http import HttpResponseNotFound
 from django.views import View
 
 # Create your views here.
@@ -37,40 +37,50 @@ def cart(request):
         for res in Product.objects.all():
             if res.id == x.id:
                 price += (res.price * x.count)
-    CartContext = {'move':Movingdata.objects.all(),'pro':Product.objects.all(),'price':price ,
-    'move_count':Movingdata.objects.count()}
+    
     Movingtoorders.objects.all().delete()
-    objs2 = Movingdata.objects.all()
-    for x in objs2:
-        if x.id != None and x.count == 0:
+
+    for x in Movingdata.objects.all():
+        restaurant = Product.objects.get(id = x.id).restaurant
+        totalamount = price + 20
+        if Movingtoorders.objects.count() == 0 and totalamount != None:
+            ordersdata = Movingtoorders(restaurant=restaurant,totalamount=totalamount)
+            ordersdata.save()
+
+        if f'trash{x.id}' in request.POST:
+            if x.count != 1:
+                id = x.id
+                restaurant = x.restaurant
+                count = x.count - 1
+                x.delete()
+                data =Movingdata.objects.create(restaurant=restaurant,id=id,count=count)
+                data.save()
+                
+            elif x.count == 1:
+                id = x.id
+                x.delete()
+            price = price - int(Product.objects.get(pk = id).price)
+                
+        if 'clearProducts' in request.POST:
             x.delete()
-        else:
-            restaurant = Product.objects.get(id = x.id).restaurant
-            totalamount = price + 20
-            if Movingtoorders.objects.count() == 0 and totalamount != None:
-                ordersdata = Movingtoorders(restaurant=restaurant,totalamount=totalamount)
-                ordersdata.save()
+            price = 0
+    
+            
+        if 'Proceed' in request.POST and price != 0:
+            ordersContext = {'movetoorders':MovedData.objects.all()}
+            orders(request)
+            return render(request,'orders/orders.html',ordersContext)
 
-            if f'trash{x.id}' in request.POST:
-                price = price -(int(x.count) * int(Product.objects.get(pk = x.id).price))
-                x.delete()
-                CartContext = {'move':Movingdata.objects.all(),'pro':Product.objects.all(),'price':price ,
-    'move_count':Movingdata.objects.count()}
-
-            if 'clearProducts' in request.POST:
-                x.delete()
-                CartContext = {'move':Movingdata.objects.all(),'pro':Product.objects.all(),'price':0,
-    'move_count':Movingdata.objects.count()}
-
-    return render(request,'cart/cart.html',CartContext)
+    cartContext = {'order_submit':False,'move':Movingdata.objects.all(),'pro':Product.objects.all(),'price':price ,'move_count':Movingdata.objects.count()}
+    return render(request,'cart/cart.html',cartContext)
 
 
-############  RESTAURANT FUNCTINS  ###############
+############  RESTAURANTS FUNCTINS  ###############
 
 httpError = """<body style="display:flex ; align-items:center ; justify-content:space-around;" ><h1>  Cannot order from two restaurants in the same time please <h1/><a href="/" style="color:green;">Resturunts</a></body>"""
 
 def restaurantviewfunc(resNum,request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant=resNum).count() != 0:
         for x in context.get('pro'):
             if x.restaurant == resNum:  
@@ -84,7 +94,7 @@ def restaurantviewfunc(resNum,request):
 ###########################################################################
 
 def restaurant1(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant1',request) 
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant='restaurant1').count() != 0:print(end='')
     else:
@@ -95,7 +105,7 @@ def restaurant1(request):
 ###########################################################################
 
 def restaurant2(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant2',request)     
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant='restaurant2').count() != 0:print()
     else:
@@ -105,7 +115,7 @@ def restaurant2(request):
 ###########################################################################
 
 def restaurant3(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant3',request)
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant='restaurant3').count() != 0:print()
     else:
@@ -115,7 +125,7 @@ def restaurant3(request):
 ###########################################################################
 
 def restaurant4(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant4',request)    
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant='restaurant4').count() != 0:print()
     else:
@@ -125,7 +135,7 @@ def restaurant4(request):
 ###########################################################################
 
 def restaurant5(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant5',request)  
     if Movingdata.objects.count() == 0 or Movingdata.objects.filter(restaurant='restaurant5').count() != 0:print()
     else:
@@ -135,7 +145,7 @@ def restaurant5(request):
 ###########################################################################
 
 def restaurant6(request):
-    context = {'pro':Product.objects.all()}
+    context = {'pro':Product.objects.all(),'moveCount':Movingdata.objects.all().count()}
     restaurantviewfunc('restaurant6',request)   
     if Movingdata.objects.count() == 0 and Movingdata.objects.filter(restaurant='restaurant6').count() != 0:print()
     else:
